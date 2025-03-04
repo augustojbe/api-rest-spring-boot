b642aeefd25e2bf4174c4dd176b420bfa128ee73
augustojbe <augustojbe>
2025-03-04 20:28:18 -0300

    terceiro commit

--- src/main/java/br/com/augustojbe/model/MathController.java
+++ src/main/java/br/com/augustojbe/model/MathController.java
@@ -33,10 +33,58 @@ public class MathController {
         return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 
     }
-
-
     // http://localhost:8080/math/subtraction/3/5
+
+    @RequestMapping("/subtraction/{numberOne}/{numberTwo}")
+    public Double subtraction(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw  new UnsupportedOperationException("Plase set a numeric value");
+        return convertDouble(numberOne)  - convertDouble(numberTwo);
+    }
+
+    // http://localhost:8080/math/multiplication/3/5
+    @RequestMapping("/multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw  new UnsupportedOperationException("Plase set a numeric value");
+        return convertDouble(numberOne) * convertDouble(numberTwo);
+    }
+
+
     // http://localhost:8080/math/division/3/5
+    @RequestMapping("/division/{numberOne}/{numberTwo}")
+    public Double division(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw  new UnsupportedOperationException("Plase set a numeric value");
+        return convertDouble(numberOne)  / convertDouble(numberTwo);
+    }
+
+    // http://localhost:8080/math/mean/3/5
+    @RequestMapping("/mean/{numberOne}/{numberTwo}")
+    public Double mean(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw  new UnsupportedOperationException("Plase set a numeric value");
+        return (convertDouble(numberOne)  / convertDouble(numberTwo)) / 2;
+    }
+
+    // http://localhost:8080/math/squareroot/3/5
+    @RequestMapping("/squareroot/{number}")
+    public Double squareRoot(
+            @PathVariable("number") String number
+    ) throws Exception {
+        if (!isNumeric(number)) throw  new UnsupportedOperationException("Plase set a numeric value");
+//        var converted = Math.sqrt(convertDouble(number));
+//        return converted;
+        return Math.sqrt(convertDouble(number));
+    }
 
 
 }
